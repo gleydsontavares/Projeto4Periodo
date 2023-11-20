ce606ed400247f8f6f6e90102fda731c2ebddd24
carolqueiroz96 <115853293+carolqueiroz96@users.noreply.github.com>
2023-11-20 04:19:40 -0300

    Adições de interface
    + adicionadas caixas de texto de objetivo na cena Tabuleiro. Ainda precisa de ajustes.
    + adicionada tela de prevenção de erro ao sair das configurações de volume sem aplicar mudanças. Ainda precisa ser adicionada a um script.
    + adicionado painel de "novo jogo" para ser adicionado a um script após pressionar o botão Jogar na cena MainMenu.
    + adicionadas tooltips nos botões de habilidade da cena da batalha por turno com descrições de cada ação.
    + alterado o estilo do painel de todas as tooltips.
    + adicionadas todas as músicas na pasta Assets/Music

--- Assets/Scripts/Tabuleiro/UIAnimator.cs
+++ Assets/Scripts/Tabuleiro/UIAnimator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class UIAnimator : MonoBehaviour
 {
     public GameObject IMG_turne;
+    public GameObject TXT_objetivo_hotel;
+    public GameObject TXT_objetivo_show;
     public GameObject IMG_moedas;
     public GameObject IMG_dia;
     public GameObject IMG_tips_panel;
@@ -13,18 +15,24 @@ public class UIAnimator : MonoBehaviour
     {
         // Pega as posições iniciais (pontos X)
         Vector3 startPosTurne = IMG_turne.transform.position;
+        Vector3 startPosHotel = TXT_objetivo_hotel.transform.position;
+        Vector3 startPosShow = TXT_objetivo_show.transform.position;
         Vector3 startPosMoedas = IMG_moedas.transform.position;
         Vector3 startPosDia = IMG_dia.transform.position;
         Vector3 startPosTips = IMG_tips_panel.transform.position;
 
         // Define as posições finais (pontos Y)
         Vector3 finalPosTurne = new Vector3(xFinalTurne, yFinalTurne, zFinalTurne);
+        Vector3 finalPosHotel = new Vector3(xFinalHotel, yFinalHotel, zFinalHotel);
+        Vector3 finalPosShow = new Vector3(xFinalShow, yFinalShow, zFinalShow);
         Vector3 finalPosMoedas = new Vector3(xFinalMoedas, yFinalMoedas, zFinalMoedas);
         Vector3 finalPosDia = new Vector3(xFinalDia, yFinalDia, zFinalDia);
         Vector3 finalPosTips = new Vector3(xFinalTips, yFinalTips, zFinalTips);
 
         // Move os botões para suas posições finais
         LeanTween.moveLocal(IMG_turne, finalPosTurne, 1.0f).setEase(LeanTweenType.easeOutBack);
+        LeanTween.moveLocal(TXT_objetivo_hotel, finalPosHotel, 1.0f).setEase(LeanTweenType.easeOutBack);
+        LeanTween.moveLocal(TXT_objetivo_show, finalPosShow, 1.0f).setEase(LeanTweenType.easeOutBack);
         LeanTween.moveLocal(IMG_moedas, finalPosMoedas, 3.0f).setEase(LeanTweenType.easeOutBack);
         LeanTween.moveLocal(IMG_dia, finalPosDia, 3.0f).setEase(LeanTweenType.easeOutBack);
         LeanTween.moveLocal(IMG_tips_panel, finalPosTips, 5.0f).setEase(LeanTweenType.easeOutBack);
@@ -34,6 +42,16 @@ public class UIAnimator : MonoBehaviour
     float xFinalTurne = -661f;
     float yFinalTurne = 388f;
     float zFinalTurne = 0f;
+
+    // Hotel
+    float xFinalHotel = -591f;
+    float yFinalHotel = 184f;
+    float zFinalHotel = 0f;
+
+    // Show
+    float xFinalShow = -591;
+    float yFinalShow = 242f;
+    float zFinalShow = 0f;
 
     // Moedas
     float xFinalMoedas = 730f;
